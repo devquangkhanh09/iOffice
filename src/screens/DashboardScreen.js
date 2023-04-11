219c1c364c7c5c56098b8f489a88a1746a58e4f1
Quang Khánh <khanhnguyenmcu@gmail.com>
2023-04-11 12:42:10 +0700

    Add selection in dashboard

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,15 +1,59 @@
 import {
     View,
+    Pressable
 } from "react-native";
 import {
     Text
 } from "@react-native-material/core";
+import { useState } from "react";
 import styles from "../styles/styles";
+import dashboardStyles from "../styles/Dashboard.styles";
 
 const DashboardScreen = () => {
+    const [data, setData] = useState("temperature");
+    const [range, setRange] = useState("1 week");
+
     return (
-        <View style={styles.screen}>
+        <View style={[styles.screen, dashboardStyles.screen]}>
             <Text variant="h3" style={styles.title}>Dashboard</Text>
+
+            <View style={dashboardStyles.selectionRow}>
+                <Pressable 
+                    onPress={() => setData("temperature")}
+                    style={[dashboardStyles.selection, data === "temperature" && dashboardStyles.selectionSelected]}
+                >
+                    <Text style={[dashboardStyles.selectionText, data === "temperature" && dashboardStyles.selectionTextSelected]}>
+                        Temperature
+                    </Text>
+                </Pressable>
+                <Pressable 
+                    onPress={() => setData("humidity")}
+                    style={[dashboardStyles.selection, data === "humidity" && dashboardStyles.selectionSelected]}
+                >
+                    <Text style={[dashboardStyles.selectionText, data === "humidity" && dashboardStyles.selectionTextSelected]}>
+                        Humidity
+                    </Text>
+                </Pressable>
+            </View>
+
+            <View style={dashboardStyles.selectionRow}>
+                <Pressable 
+                    onPress={() => setRange("1 week")}
+                    style={[dashboardStyles.selection, range === "1 week" && dashboardStyles.selectionSelected]}
+                >
+                    <Text style={[dashboardStyles.selectionText, range === "1 week" && dashboardStyles.selectionTextSelected]}>
+                        1 week
+                    </Text>
+                </Pressable>
+                <Pressable 
+                    onPress={() => setRange("1 day")}
+                    style={[dashboardStyles.selection, range === "1 day" && dashboardStyles.selectionSelected]}
+                >
+                    <Text style={[dashboardStyles.selectionText, range === "1 day" && dashboardStyles.selectionTextSelected]}>
+                        1 day
+                    </Text>
+                </Pressable>
+            </View>
         </View>
     );
 }
---
+++ src/styles/Dashboard.styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+const dashboardStyles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        rowGap: 15,
+    },
+    selectionRow: {
+        height: "5%",
+        flexDirection: "row",
+        borderWidth: 1,
+        borderColor: "#4287f5",
+        justifyContent: "center",
+    },
+    selection: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    selectionText: {
+        fontSize: 16,
+    },
+    selectionSelected: {
+        backgroundColor: "#4287f5",
+    },
+    selectionTextSelected: {
+        color: "#fafbfc",
+    },
+});
+
+export default dashboardStyles;
