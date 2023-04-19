f09194dbd217105570f16fe83868db0647e81e1e
Prosecutor22 <phanphuocminh2002@gmail.com>
2023-04-19 17:28:02 +0700

    chart done

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -33,50 +33,29 @@ const prefixData = "metacrektal/feeds/iot-data.data-"
 
 const DashboardScreen = () => {
     const [type, setType] = useState(null);
+    const [typeC, setTypeC] = useState(null);
     const [date, setDate] = useState(new Date());
-    const [mode, setMode] = useState('date');
     const [show, setShow] = useState(false);
-    const [text, setText] = useState('Choosen date:' + date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear());
-    const [dataTemp, setDataTemp] = useState(null);
-    const [dataHumd, setDataHumd] = useState(null);
-    const [dataMax, setDataMax] = useState([0, null, null, null, null, null, null, null, null, null, null, null]);
-    const [dataMin, setDataMin] = useState([0, null, null, null, null, null, null, null, null, null, null, null]);
-
-    var data = {
-        labels: ["7h", "8h", "9h", "10h", "11h", "12h", "13h", "14h", "15h", "16h", "17h"],
-        datasets: [
-          {
-            data: dataMax,
-            color: (opacity = 1) => `rgba(3, 40, 252, ${opacity})`,
-            strokeWidth: 2
-          },
-          {
-            data: dataMin,
-            color: (opacity = 1) => `rgba(255, 165, 0, ${opacity})`,
-            strokeWidth: 2
-          }
-        ],
-        legend: type === 'temp' ? ["Temperature"] : ["Humidity"]
-    };
-    useEffect(() => {
-        fetch(`${baseUrl}/${prefixData}temp/data?limit=2000`, {
-            method: "GET",
-            headers: {
-                "X-AIO-Key": AIO_KEY
-            },
-        }).then((res) => res.json()).then((data) => {
-            setDataTemp(data);
-        }).catch((e) => console.log(e));
-        fetch(`${baseUrl}/${prefixData}humd/data?limit=2000`, {
-            method: "GET",
-            headers: {
-                "X-AIO-Key": AIO_KEY
-            },
-        }).then((res) => res.json()).then((data) => {
-            setDataHumd(data);
-        }).catch((e) => console.log(e));
-        console.log("Fetch data done");
-    }, []);
+    // const [dataTemp, setDataTemp] = useState(null);
+    // const [dataHumd, setDataHumd] = useState(null);
+    // const [dataMax, setDataMax] = useState([0, null, null, null, null, null, null, null, null, null, null, null]);
+    // const [dataMin, setDataMin] = useState([0, null, null, null, null, null, null, null, null, null, null, null]);
+    const [data, setData] = useState({
+            labels: ["7h", "8h", "9h", "10h", "11h", "12h", "13h", "14h", "15h", "16h", "17h"],
+            datasets: [
+              {
+                data: [0, null, null, null, null, null, null, null, null, null, null, null],
+                color: (opacity = 1) => `rgba(3, 40, 252, ${opacity})`,
+                strokeWidth: 2
+              },
+              {
+                data: [0, null, null, null, null, null, null, null, null, null, null, null],
+                color: (opacity = 1) => `rgba(255, 165, 0, ${opacity})`,
+                strokeWidth: 2
+              }
+            ],
+            legend: ["Temperature"]
+    })
 
     const onChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
@@ -85,12 +64,8 @@ const DashboardScreen = () => {
         let tempDate = new Date(currentDate);
         let fDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear();
         setText('Choosen date:' + fDate); 
+        console.log(dataMax, dataMin);
         setType(null);
-    }
-
-    const showMode = (currentMode) => {
-        setShow(true);
-        setMode(currentMode);
     }
 
     const filterData = (curtype, date) => {
@@ -99,8 +74,8 @@ const DashboardScreen = () => {
          } else {
             res = dataHumd;
         }
-        var dtMax = [0, null, null, null, null, null, null, null, null, null, null, null];
-        var dtMin = [0, null, null, null, null, null, null, null, null, null, null, null];
+        var dtMax = [0, null, null, null, null, null, null, null, null, null, null];
+        var dtMin = [0, null, null, null, null, null, null, null, null, null, null];
         res.forEach((record) => {
             const curDate = new Date(record["created_at"]);
             const value = parseFloat(record["value"]);
@@ -111,24 +86,37 @@ const DashboardScreen = () => {
                     if (dtMax[idx - 7] < value) dtMax[idx - 7] = value;
                     if (dtMin[idx - 7] === null) dtMin[idx - 7] = value;
                     if (dtMin[idx - 7] > value) dtMin[idx - 7] = value;
+                    if (dtMin[idx - 7] === 0) dtMin[idx - 7] = value;
                 }
             }
         });
         setDataMax(dtMax);
         setDataMin(dtMin);
+        setType(curtype);
     }
 
+    const convert = (curDate) => {
+        return String(curDate.getFullYear()) + '-' + String(curDate.getMonth() + 1).padStart(2, '0') + '-' + String(curDate.getDate()).padStart(2, '0') + 'T00:00Z';
+    }
+
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    }
     return (
         <View style={[styles.screen, dashboardStyles.screen]}>
             <Text variant="h3" style={styles.title}>Dashboard</Text>
-            <Text style={{fontWeight:'bold', fontSize: 20}}>{text}</Text>
+            <Text style={{fontWeight:'bold', fontSize: 20}}>{'Choosen date:' + date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()}</Text>
             <View style = {{margin: 20}}>
-                <Button title='Choose Date' onPress={() => showMode('date')}></Button>
+                <Button title='Choose Date' onPress={() => {
+                    setShow(true);
+                    setTypeC(null);
+                }}></Button>
                 {show && (
                     <DateTimePicker
                     testID="DateTimePicker"
                     value={date}
-                    mode={mode}
+                    mode={'date'}
                     is24Hour={true}
                     display="default"
                     onChange={onChange}
@@ -138,25 +126,8 @@ const DashboardScreen = () => {
             <View style={dashboardStyles.selectionRow}>
                 <Pressable 
                     onPress={() => {
-                        setType("temp");
-                        var dtMax = [0, null, null, null, null, null, null, null, null, null, null];
-                        var dtMin = [0, null, null, null, null, null, null, null, null, null, null];
-                        dataTemp.forEach((record) => {
-                            const curDate = new Date(record["created_at"]);
-                            const value = parseFloat(record["value"]);
-                            if (curDate.getDate() === date.getDate() && curDate.getMonth() === date.getMonth() && curDate.getFullYear() === date.getFullYear()) {
-                                var idx = curDate.getHours();
-                                if (idx <= 17 && idx >= 7){
-                                    if (dtMax[idx - 7] === null) dtMax[idx - 7] = value;
-                                    if (dtMax[idx - 7] < value) dtMax[idx - 7] = value;
-                                    if (dtMin[idx - 7] === null) dtMin[idx - 7] = value;
-                                    if (dtMin[idx - 7] > value) dtMin[idx - 7] = value;
-                                    if (dtMin[idx - 7] === 0) dtMin[idx - 7] = value;
-                                }
-                            }
-                        });
-                        setDataMax(dtMax);
-                        setDataMin(dtMin);
+                        setType('temp');  
+                        setTypeC(null);                      
                     }}
                     style={[dashboardStyles.selection, type === "temp" && dashboardStyles.selectionSelected]}
                 >
@@ -167,25 +138,8 @@ const DashboardScreen = () => {
                 </Pressable>
                 <Pressable 
                     onPress={() => {
-                        setType("humd");
-                        var dtMax = [0, null, null, null, null, null, null, null, null, null, null];
-                        var dtMin = [0, null, null, null, null, null, null, null, null, null, null];
-                        dataHumd.forEach((record) => {
-                            const curDate = new Date(record["created_at"]);
-                            const value = parseFloat(record["value"]);
-                            if (curDate.getDate() === date.getDate() && curDate.getMonth() === date.getMonth() && curDate.getFullYear() === date.getFullYear()) {
-                                var idx = curDate.getHours();
-                                if (idx <= 17 && idx >= 7){
-                                    if (dtMax[idx - 7] === null) dtMax[idx - 7] = value;
-                                    if (dtMax[idx - 7] < value) dtMax[idx - 7] = value;
-                                    if (dtMin[idx - 7] === null) dtMin[idx - 7] = value;
-                                    if (dtMin[idx - 7] > value) dtMin[idx - 7] = value;
-                                    if (dtMin[idx - 7] === 0) dtMin[idx - 7] = value;
-                                }
-                            }
-                        });
-                        setDataMax(dtMax);
-                        setDataMin(dtMin);
+                        setType('humd');
+                        setTypeC(null);  
                     }}
                     style={[dashboardStyles.selection, type === "humd" && dashboardStyles.selectionSelected]}
                 >
@@ -194,25 +148,44 @@ const DashboardScreen = () => {
                     </Text>
                 </Pressable>
             </View>
+            <Button title="Find" onPress={() => {
+                var start = convert(date);
+                var end = convert(new Date(date.getTime() + (24 * 60 * 60 * 1000)));
+                fetch(`${baseUrl}/${prefixData}${type}/data?start_time=${start}&end_time=${end}&limit=1000`, {
+                    method: "GET",
+                    headers: {
+                        "X-AIO-Key": AIO_KEY
+                    }
+                }).then((res) => res.json()).then((res) => {
+                    var dtMax = [0, null, null, null, null, null, null, null, null, null, null];
+                    var dtMin = [0, null, null, null, null, null, null, null, null, null, null];
+                    res.forEach((record) => {
+                        const value = parseFloat(record["value"]);
+                        var curDate = new Date(record["created_at"]);
+                        var idx = curDate.getHours();
+                        if (idx <= 17 && idx >= 7){
+                            if (dtMax[idx - 7] === null) dtMax[idx - 7] = value;
+                            if (dtMax[idx - 7] < value) dtMax[idx - 7] = value;
+                            if (dtMin[idx - 7] === null) dtMin[idx - 7] = value;
+                            if (dtMin[idx - 7] > value) dtMin[idx - 7] = value;
+                            if (dtMin[idx - 7] === 0) dtMin[idx - 7] = value;
+                        }
+                    });
+                    setData(pre => {
+                        pre.datasets[0].data = dtMax;
+                        pre.datasets[1].data = dtMin;
+                        pre.legend = (type === 'temp') ? ['Temperature'] : ['Humidity'];
+                        // console.log(pre.datasets);
+                        return pre;
+                    })
+                    console.log(data.datasets);
+                    setTypeC(true);
+                }).catch((e) => console.log(e));
+            }}/>
             
-            {type !== null ? (
+            {typeC !== null ? (
             <LineChart
-                data={{
-                    labels: ["7h", "8h", "9h", "10h", "11h", "12h", "13h", "14h", "15h", "16h", "17h"],
-                    datasets: [
-                      {
-                        data: dataMax,
-                        color: (opacity = 1) => `rgba(3, 40, 252, ${opacity})`,
-                        strokeWidth: 2
-                      },
-                      {
-                        data: dataMin,
-                        color: (opacity = 1) => `rgba(255, 165, 0, ${opacity})`,
-                        strokeWidth: 2
-                      }
-                    ],
-                    legend: type === 'temp' ? ["Temperature"] : ["Humidity"]                   
-                }}
+                data={data}
                 width={Dimensions.get("window").width - 50}
                 height={220}
                 chartConfig={chartConfig}
