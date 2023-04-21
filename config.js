bb6ac35454b0376c1522bf3ce857aee091593a99
Minh Phan Phuoc <minh.phanphuocminh@hcmut.edu.vn>
2023-04-21 22:52:22 +0700

    add db for app

--- config.js
+++
@@ -1,5 +0,0 @@
-var mqtt = require('mqtt');
-require('dotenv').config();
-
-exports.aioSensor = mqtt.connect(`mqtts://${process.env.AIO_USERNAME}:${process.env.AIO_KEY}@io.adafruit.com`, 8883)
-exports.aioControl = mqtt.connect(`mqtts://${process.env.AIO_USERNAME}:${process.env.AIO_KEY}@io.adafruit.com`, 8883)
